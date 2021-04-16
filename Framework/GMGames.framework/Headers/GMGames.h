02949ece8629ed4b67318a69d836a0140572517c
MWS <18824676521@163.com>
2021-04-16 09:51:08 +0800

    提交Framework和Bundle

---
+++ Framework/GMGames.framework/Headers/GMGames.h
@@ -0,0 +1,19 @@
+//
+//  GMGames.h
+//  GMGames
+//
+//  Created by Mgs on 2021/4/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GMGames.
+FOUNDATION_EXPORT double GMGamesVersionNumber;
+
+//! Project version string for GMGames.
+FOUNDATION_EXPORT const unsigned char GMGamesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMGames/PublicHeader.h>
+#import <GMGames/GamaTools.h>
+#import <GMGames/GamaCore.h>
+
